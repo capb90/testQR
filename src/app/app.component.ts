6242c702505d7fcab080c4093c0b3568b5f67e67
capb90 <capb90@hotmail.com>
2021-05-14 08:15:38 -0500

    Feat: :zap: canvas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,42 +22,39 @@ export class AppComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-  //   this.qrScannerComponent.capturedQr.subscribe(result => {
-  //     console.log(result);
-      
-  // });
+    // this.qrScannerComponent.startScanning(null);
 
   }
 
   ngAfterViewInit(): void {
-    // this.qrScannerComponent.getMediaDevices().then(devices => {
-    //     const videoDevices: MediaDeviceInfo[] = [];
-    //     for (const device of devices) {
-    //         if (device.kind.toString() === 'videoinput') {
-    //             videoDevices.push(device);
-    //         }
-    //     }
-    //     if (videoDevices.length > 0){
-    //         let choosenDev;
-    //         for (const dev of videoDevices){
-    //             if (dev.label.includes('back')){
-    //                 choosenDev = dev;
-    //                 break;
-    //             }
-    //         }
-    //         if (choosenDev) {
-    //             this.qrScannerComponent.chooseCamera.next(choosenDev);
-    //         } else {
-    //             this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
-    //         }
-    //     }
-    // });
+    this.qrScannerComponent.getMediaDevices().then(devices => {
+        const videoDevices: MediaDeviceInfo[] = [];
+        for (const device of devices) {
+            if (device.kind.toString() === 'videoinput') {
+                videoDevices.push(device);
+            }
+        }
+        if (videoDevices.length > 0){
+            let choosenDev;
+            for (const dev of videoDevices){
+                if (dev.label.includes('back')){
+                    choosenDev = dev;
+                    break;
+                }
+            }
+            if (choosenDev) {
+                this.qrScannerComponent.chooseCamera.next(choosenDev);
+            } else {
+                this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
+            }
+        }
+    });
 
-    this.qrScannerComponent.startScanning(null);
+   
 
-    // this.qrScannerComponent.capturedQr.subscribe(dataQR => {
-    //  console.log(dataQR)
-    // });
+    this.qrScannerComponent.capturedQr.subscribe(dataQR => {
+     console.log(dataQR)
+    });
    
      
   }

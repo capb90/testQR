8731b91ed10521ba0b47436e6dd692a531bc90de
capb90 <capb90@hotmail.com>
2022-01-26 15:39:18 -0500

    🚧 Work in Progress:  Dynamic points canvas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,49 +14,55 @@ import { QrScannerComponent } from 'angular2-qrscanner';
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent implements OnInit {
-    
   scanResult;
-  @ViewChild(QrScannerComponent,{static:false}) qrScannerComponent: QrScannerComponent ;
+  @ViewChild(QrScannerComponent, { static: false })
+  qrScannerComponent: QrScannerComponent;
 
-  
+  get canvasWidth(): number {
+    return window.outerHeight;
+  }
+
+  get canvasHeight(): number {
+    return window.outerHeight;
+  }
+
   constructor() {}
 
   ngOnInit() {
-    
+    console.log('🚀 canvasWidth', this.canvasWidth);
+    console.log('🚀 canvasHeight', this.canvasHeight);
   }
 
-
-    ngAfterViewInit(): void {
-      this.qrScannerComponent.getMediaDevices().then(devices => {
-          const videoDevices: MediaDeviceInfo[] = [];
-          for (const device of devices) {
-              if (device.kind.toString() === 'videoinput') {
-                  videoDevices.push(device);
-              }
+  ngAfterViewInit(): void {
+    this.qrScannerComponent.getMediaDevices().then((devices) => {
+      const videoDevices: MediaDeviceInfo[] = [];
+      for (const device of devices) {
+        if (device.kind.toString() === 'videoinput') {
+          videoDevices.push(device);
+        }
+      }
+      if (videoDevices.length > 0) {
+        console.log('🚀 videoDevices', videoDevices);
+        let choosenDev;
+        for (const dev of videoDevices) {
+          if (dev.label.includes('back')) {
+            choosenDev = dev;
+            break;
           }
-          if (videoDevices.length > 0){
-              console.log("🚀 videoDevices", videoDevices)
-              let choosenDev;
-              for (const dev of videoDevices){
-                  if (dev.label.includes('back')){
-                      choosenDev = dev;
-                      break;
-                  }
-              }
-              console.log("🚀 choosenDev", choosenDev)
-              if (choosenDev) {
-                  this.qrScannerComponent.chooseCamera.next(choosenDev);
-              } else {
-                  this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
-              }
-          }
-      });
+        }
+        console.log('🚀 choosenDev', choosenDev);
+        if (choosenDev) {
+          this.qrScannerComponent.chooseCamera.next(choosenDev);
+        } else {
+          this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
+        }
+      }
+    });
 
-      this.qrScannerComponent.capturedQr.subscribe(dataQR => {
-       console.log(dataQR);
-       this.scanResult=dataQR;
-       //window.location.href=dataQR
-      });
-
-    }
+    this.qrScannerComponent.capturedQr.subscribe((dataQR) => {
+      console.log(dataQR);
+      this.scanResult = dataQR;
+      //window.location.href=dataQR
+    });
+  }
 }
